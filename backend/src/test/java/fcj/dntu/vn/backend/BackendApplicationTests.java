a257103287a6a9b5174ed8faf62b92fccf7752b9
minhnghia2k3 <brozennvn@gmail.com>
2025-02-02 15:59:33 +0700

    Bootstrap Project in 2025 🧧🚀

---
+++ backend/src/test/java/fcj/dntu/vn/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fcj.dntu.vn.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
