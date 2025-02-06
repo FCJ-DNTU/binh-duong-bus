574cbf3180bf218d85b8b9cb925e6aec4f711701
nhungnguyen-9 <nguyennhung9846@gmail.com>
2025-02-06 13:26:44 +0700

    feat: add repository folder

--- backend/src/main/java/fcj/dntu/vn/backend/repositories/RouteRepository.java
+++ backend/src/main/java/fcj/dntu/vn/backend/repositories/RouteRepository.java
@@ -2,8 +2,10 @@ package fcj.dntu.vn.backend.repositories;
 
 import fcj.dntu.vn.backend.models.RouteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface RouteRepository extends JpaRepository<RouteModel, UUID> {
 }
--- backend/src/main/java/fcj/dntu/vn/backend/repositories/TimeLineRepository.java
+++ backend/src/main/java/fcj/dntu/vn/backend/repositories/TimeLineRepository.java
@@ -1,11 +1,18 @@
 package fcj.dntu.vn.backend.repositories;
 
 import fcj.dntu.vn.backend.models.TimeLineModel;
+import fcj.dntu.vn.backend.models.enums.DirectionEnum;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TimeLineRepository extends JpaRepository<TimeLineModel, UUID> {
+
+    List<TimeLineModel> findByRouteIdAndDirection(UUID routeId, DirectionEnum direction);
+
+    List<TimeLineModel> findByRouteId(UUID routeId);
 }
