0b6a615d06958621bac16b7142fdfacdf61f3050
minhnghia2k3 <BrozennVN@gmail.com>
2025-03-08 13:01:04 +0700

    chore: delete generated flutter code

--- backend/src/main/java/fcj/dntu/vn/backend/repositories/WayGeometryRepository.java
+++
@@ -1,9 +0,0 @@
-package fcj.dntu.vn.backend.repositories;
-
-import fcj.dntu.vn.backend.models.WayGeometryModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface WayGeometryRepository extends JpaRepository<WayGeometryModel, UUID> {
-}
